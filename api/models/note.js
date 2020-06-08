06ad2df3a12aff8ab3a80fddafeaf2c56fcedef3
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2020-06-08 16:40:37 +0530

    Final

--- api/models/note.js
+++ api/models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const noteSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    title : String,
+    title : {type: String, required : true},
     content : String
     
 });
